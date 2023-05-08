e3c5800eeee717558b6e3725107c60410829ca24
ale7santos <alessandro.jdsantos@gmail.com>
2023-05-08 11:27:51 -0300

    last commit

--- Exercicio-03.js
+++ Exercicio-03.js
@@ -6,4 +6,59 @@ c. Apenas a parte inteira
 d. O número arredondado para baixo
 e. O número arredondado para cima*/
 
-let n = [1,2,3,4,5];
+/*
+let n = [1, 2, 3, 4, 5];
+
+let i = 0;
+
+/*          --- a. O quadrado ---*
+let quadrado = 0;
+
+while (i < 5) {
+    quadrado = n[i] * n[i];
+    console.log(`O quadrado de ${n[i]} é: ${quadrado}`);
+    i++;
+          
+}*/
+
+/*          --- b. A raiz quadrada ---*
+while (i < 5) {
+console.log(Math.sqrt(n[i]));
+i++;
+}*/
+
+
+/*          --- c. Somente Inteiros ---*
+let n = [1.2, 2.4, 3.5, 4.7, 5.8];
+let i = 1;
+
+
+while(i<=5){
+console.log(Math.trunc(i));
+i++
+}
+*/
+
+/*          --- d. Arrendonda para baixo ---*
+
+/*
+let n = [1.2456, 2.46565, 3.55433, 4.75656, 5.87676];
+let i = 1;
+
+
+while(i<=5){
+console.log(Math.floor(i));
+i++
+}
+*/
+
+/*          --- d. Arrendonda para cima ---*
+let n = [1.2456, 2.46565, 3.55433, 4.75656, 5.87676];
+let i = 1;
+
+
+while(i<=5){
+console.log(Math.ceil(i));
+i++
+}
+*/
